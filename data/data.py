d757d1f52b4b17d12b428afe3c1bd15f2b6a4b07
harrisson-li <harrisson.li@outlook.com>
2021-04-02 10:13:43 +0800

    add data

---
+++ data/data.py
@@ -0,0 +1,2 @@
+corporate_login_data = {'user_name': 'test_1@cbim.com.cn', 'company': 'devgraph', 'password': 'wasd1234'}
+individual_login_data = {'user_name': '13661666132', 'password': 'wasd1234'}
